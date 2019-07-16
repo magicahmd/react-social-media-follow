00bd7bf0ccbf25bd9897b9861f0f9dc69b6f922b
Ahmad Salhi <asalhi@asaltech.com>
2019-07-16 15:07:10 +0300

    Updates

---
+++ precache-manifest.a61712e81a67fc790c3a50ba090d26cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d7dbefa3e5ddbe0b55467e0015e701",
+    "url": "/magicahmd/react-social-media-follow/index.html"
+  },
+  {
+    "revision": "8a92b3a9676f5c1a01f8",
+    "url": "/magicahmd/react-social-media-follow/static/css/main.eefd6595.chunk.css"
+  },
+  {
+    "revision": "53bcf7ca3de013a30b61",
+    "url": "/magicahmd/react-social-media-follow/static/js/2.df7f72e2.chunk.js"
+  },
+  {
+    "revision": "8a92b3a9676f5c1a01f8",
+    "url": "/magicahmd/react-social-media-follow/static/js/main.8d82ab70.chunk.js"
+  },
+  {
+    "revision": "bb66c462a343c045a977",
+    "url": "/magicahmd/react-social-media-follow/static/js/runtime~main.5a32e1c8.js"
+  }
+]);

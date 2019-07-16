b9024d90d221713279db544356ed29fed6a87374
Ahmad Salhi <asalhi@asaltech.com>
2019-07-16 14:53:08 +0300

    create demo.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import FollowAt from "react-social-media-follow";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Follow me at:</p>
+      <FollowAt links = {[
+      'https://twitter.com/magicahmd',
+      'https://www.facebook.com/ahmdsalhi',
+      'https://www.youtube.com/magicahmd',
+      'https://www.instagram.com/ahmdsalhi',
+    ]}/>
     </div>
   );
 }

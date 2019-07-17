90ccc14488d75268b2b410c131fc86c3d8168fc7
Ahmad Salhi <asalhi@asaltech.com>
2019-07-17 10:31:28 +0300

    use extract-domain packege + remove padding + add more websites.

--- src/index.js
+++ src/index.js
@@ -1,70 +1,72 @@
 import React from "react";
 import { css } from "emotion";
 import withStyles from "withstyles";
-import PropTypes from 'prop-types'; // ES6
+import PropTypes from 'prop-types';
+import extractDomain from 'extract-domain';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faYoutube,
   faFacebook,
   faTwitter,
   faInstagram,
-  faTumblr
+  faTumblr,
+  faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 
-const domainName = url => {
-  var urlPattern = /^(?:https?:\/\/)?(?:w{3}\.)?([a-z\d\.-]+)\.(?:[a-z\.]{2,10})(?:[/\w\.-]*)*/;
-  var domainPattern = url.match(urlPattern);
-  var extractDomain = domainPattern[1];
-  return extractDomain.charAt(0).toUpperCase() + extractDomain.slice(1);
-};
 
 const getIcon = domain => {
   switch (domain) {
-    case "Facebook":
+    case "facebook.com":
       return faFacebook;
-    case "Twitter":
+    case "twitter.com":
       return faTwitter;
-    case "Youtube":
+    case "youtube.com":
       return faYoutube;
-    case "Instagram":
+    case "instagram.com":
       return faInstagram;
-    case "Tumblr":
+    case "tumblr.com":
       return faTumblr;
+    case "github.com":
+      return faGithub;
+    default:
+      return;
   }
 };
 
 const styles = ({ color, hoverColor, hoverMove }) => ({
   socialContainer: css`
-    padding: 25px 50px;
   `,
 
   social: css`
-    margin: 0 1rem;
+    margin: 0.8rem;
     transition: transform 250ms;
     display: inline-block;
+    color: black;
+    opacity: 0.8;
     &:hover {
       transform: ${hoverMove===undefined && 'translateY(-2px)' || hoverMove ? 'translateY(-2px)' : ''};
       color: ${hoverColor};
+      opacity: 1;
     }
   `,
 
-  Facebook: css`
+  'facebook.com': css`
     color: ${color || "#4968ad"};
   `,
 
-  Youtube: css`
+  'youtube.com': css`
     color: ${color || "#eb3223"};
   `,
 
-  Twitter: css`
+  'twitter.com': css`
     color: ${color || "#49a1eb"};
   `,
 
-  Instagram: css`
+  'instagram.com': css`
     color: ${color || "black"};
   `,
 
-  Tumblr: css`
+  'tumblr.com': css`
     color: ${color || "#35465c"};
   `
 });
@@ -73,15 +75,15 @@ const FollowAt = props => {
   const { computedStyles, links, iconSize } = props;
   return (
     <div className={computedStyles.socialContainer}>
-      {links.map((link,index) => {
-        let socailMedia = domainName(link);
-        let icon = getIcon(socailMedia);
+      {links.map((link) => {
+        let socialMedia = String(extractDomain(link)).toLowerCase();
+        let icon = getIcon(socialMedia);
         return (
           <a
-            key={index}
+            key={socialMedia}
             href={link}
             className={`${computedStyles.social} ${
-              computedStyles[socailMedia]
+              computedStyles[ socialMedia ]
             }`}
           >
             <FontAwesomeIcon icon={icon} size={`${iconSize}x`} />
